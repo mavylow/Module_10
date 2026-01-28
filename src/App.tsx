8a0dc355c1c982c50dddf537234e335db3d45db5
sofia maiseenko <sofa.majseenko@gmail.com>
2026-01-28 16:00:31 +0300

    switch to HashRouter

--- src/App.tsx
+++ src/App.tsx
@@ -6,25 +6,26 @@ import ThemeProvider from "@/ThemeProvider";
 import ErrorBoundary from "./components/ ErrorBoundary";
 
 import SignUp from "./pages/auth/SignUp";
-import { BrowserRouter, Route, Routes } from "react-router";
+import { HashRouter, Route, Routes } from "react-router";
 import SignIn from "./pages/auth/SignIn";
 import Home from "./pages/Home";
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <ErrorBoundary>
         <AuthProvider>
           <ThemeProvider>
             <Routes>
-              <Route path="/Module_10/signin" element={<SignIn />} />
-              <Route path="/Module_10/signup" element={<SignUp />} />
-              <Route path="/Module_10/home" element={<Home />} />
+              <Route path="/signin" element={<SignIn />} />
+              <Route path="/signup" element={<SignUp />} />
+              <Route path="/home" element={<Home />} />
+              <Route path="/" element={<Home />} />
             </Routes>
           </ThemeProvider>
         </AuthProvider>
       </ErrorBoundary>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
