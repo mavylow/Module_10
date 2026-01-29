f9aefc79a7aa8c617c8244e2d58a979786613014
sofia maiseenko <sofa.majseenko@gmail.com>
2026-01-29 14:49:40 +0300

    connected to msw

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,16 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+import { startMockingSocial } from "@sidekick-monorepo/internship-backend";
+
+async function enableMocking() {
+  await startMockingSocial("");
+}
+
+enableMocking().then(() => {
+  createRoot(document.getElementById("root")!).render(
+    <StrictMode>
+      <App />
+    </StrictMode>
+  );
+});
